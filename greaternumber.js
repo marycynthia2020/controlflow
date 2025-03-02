4db7195ed3a41271450710afba766d250d4272ac
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2025-03-02 16:11:44 +0100

    adds equality comparison to greater than function

--- greaternumber.js
+++ greaternumber.js
@@ -1,7 +1,9 @@
 // using if/else statement
 
 function greaterNumber(num1, num2) {
-  if (num1 > num2) {
+  if (num1 == num2) {
+    console.log(`${num1} and ${num2} are equal`);
+  } else if (num1 > num2) {
     console.log(`${num1} is greater than ${num2}`);
   } else {
     console.log(`${num2} is greater than ${num1}`);
@@ -9,10 +11,12 @@ function greaterNumber(num1, num2) {
 }
 
 // using ternary operator
-function greaterNumberT(num1, num2) {
-  num1 > num2
-    ? `${num1} is greater than ${num2}`
-    : `${num2} is greater than ${num1}`;
-}
+// function greaterNumberT(num1, num2) {
+//   num1 == num2
+//     ? console.log(`${num1} and ${num2} are equal`)
+//     : num1 > num2
+//     ? console.log(`${num1} is greater than ${num2}`)
+//     : console.log(`${num2} is greater than ${num1}`);
+// }
 
-greaterNumber(150, 8000);
+greaterNumber(1502, 1502);
