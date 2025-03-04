e21438cfcfdf8d04543334c068e68c36cb8d68cc
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2025-03-04 13:44:04 +0100

    adds validation

--- gradecheck.js
+++ gradecheck.js
@@ -1,26 +1,26 @@
 // Write a program that transforms a numerical grade to a letter grade (0-44: F, 45-49: D, 50-59: C, 60-69: B, 70-100: A).
 
-// function grade(score) {
-//   if (score > 100 || score < 0) {
-//     console.log("invalid score");
-//   } else if (score >= 70) {
-//     console.log(`your score is ${score} and your grade is an A`);
-//   } else if (score >= 60) {
-//     console.log(`your score is ${score} and your grade is a B`);
-//   } else if (score >= 50) {
-//     console.log(`your score is ${score} and your grade is a C`);
-//   } else if (score >= 45) {
-//     console.log(`your score is ${score} and your grade is a D`);
-//   } else {
-//     console.log(`your score is ${score} and your grade is an F`);
-//   }
-// }
+function grade(score) {
+  if (score > 100 || score < 0) {
+    console.log("invalid score");
+  } else if (score >= 70) {
+    console.log(`your score is ${score} and your grade is an A`);
+  } else if (score >= 60) {
+    console.log(`your score is ${score} and your grade is a B`);
+  } else if (score >= 50) {
+    console.log(`your score is ${score} and your grade is a C`);
+  } else if (score >= 45) {
+    console.log(`your score is ${score} and your grade is a D`);
+  } else {
+    console.log(`your score is ${score} and your grade is an F`);
+  }
+}
 
-// // example
+// example
 
-// grade(44);
-// grade(-1);
-// grade(122);
+grade(44);
+grade(-1);
+grade(122);
 
 let score = 65;
 
